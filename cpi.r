e5ce6bf6639c06567b3d24361dc0f96e08c63303
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-05-15 21:41:48 +0530

    corrected labelling

--- cpi.r
+++ cpi.r
@@ -1,6 +1,6 @@
 spi = c(9.7, 9.71, 9.8, 10, 9.69, 9.75, 9.8, 10)
 cpi = mean(spi)
 sem = c("Sem 1","Sem 2","Sem 3","Sem 4","Sem 5","Sem 6","Sem 7","Sem 8")
-names(cpi) = sem
+names(spi) = sem
 sel = spi>cpi
 sel 
